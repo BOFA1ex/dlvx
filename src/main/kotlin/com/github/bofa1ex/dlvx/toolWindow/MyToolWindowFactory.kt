cdecbe8f4cd86fb88b156371d6b39e85b3b9ebb3
GitHub Action <action@github.com>
2024-01-02 11:34:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/bofa1ex/dlvx/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bofa1ex/dlvx/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bofa1ex.dlvx.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bofa1ex.dlvx.MyBundle
+import com.github.bofa1ex.dlvx.services.MyProjectService
 import javax.swing.JButton
 
 
